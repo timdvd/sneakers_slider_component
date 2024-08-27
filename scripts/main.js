a0e0c31b63ee38c59c63167e2ac3149c17270e19
timdvd <76249593+timdvd@users.noreply.github.com>
2024-08-27 18:44:34 +0300

    commit

---
+++ scripts/main.js
@@ -0,0 +1,56 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* remove menu mobile */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/* swiper */
+const swiper = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    gradCursor: true,
+    effect: 'creative',
+    creativeEffect: {
+        prev: {
+            translate: [-100, 0, -500],
+            rotate: [0, 0, 15],
+            opacity: 0
+        },
+        next: {
+            translate: [100, 0, -500],
+            rotate: [0, 0, -15],
+            opacity: 0
+        },
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
+
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
